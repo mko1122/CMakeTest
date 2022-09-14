1dbcdb176b61f7debd77b47e5b6df4ab81bdfa7f
Samuel Rojko <samuelrojko1@gmail.com>
2022-09-14 22:13:40 +0200

    Episode2 code

---
+++ Ep002/source/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template <typename T>
+requires std::integral<T>
+T add(T a, T b)
+{
+	return a + b;
+}
+
+int main()
+{
+	std::cout << "Hello from C++ 20 with CMake on Linux/Windows" << std::endl;
+	std::cout << "The sum is : " << add(7,3) << std::endl;
+	return 0;
+}
